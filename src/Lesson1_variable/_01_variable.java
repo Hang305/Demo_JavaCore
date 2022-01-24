6a849b79cab26d891038bc86eaa8afe4c77535ab
hang305 <hanglee305@gmail.com>
2022-01-24 11:49:28 +0700

    Variable

---
+++ src/Lesson1_variable/_01_variable.java
@@ -0,0 +1,23 @@
+package Lesson1_variable;
+
+public class _01_variable {
+
+    public static void main(String[] args) {
+
+        String name = "Le Thi Thanh Hang";
+        int yearofBirth = 2000;
+
+        int number = 30;
+        number =20;
+
+        final double pi = 3.14;
+//        pi =20;
+
+        System.out.println(name);
+        System.out.println("Welcome " + name);
+        System.out.println("My year of birth is " +yearofBirth);
+
+        System.out.println("The number is " +number);
+        System.out.println("The pi is " +pi);
+    }
+}
