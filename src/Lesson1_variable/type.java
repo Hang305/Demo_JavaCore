5b22e8fa83c15283ac6090c3302733ffbb2721e7
hang305 <hanglee305@gmail.com>
2022-01-24 12:06:13 +0700

    Type

---
+++ src/Lesson1_variable/type.java
@@ -0,0 +1,37 @@
+package Lesson1_variable;
+
+public class type {
+
+    public static void main(String[] args) {
+
+        //int
+        int number = 912472758;
+
+        //char
+        char a =105, b=66 , c=67;
+
+        //boolean
+        boolean isCorrect = true;
+        boolean isPrint = false;
+
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+
+        //string
+        String fullname ="Le Thi Thanh Hang";
+
+        //ọbject
+        type name = new type();
+
+        System.out.println("The number is " +number);
+        System.out.println(a + " " +b +" " +c);
+        System.out.println("The field is "+ isCorrect);
+        System.out.println(f1);
+        System.out.println(d1);
+        System.out.println(f1 + d1);
+        System.out.println("My fullname is "+fullname);
+        System.out.println(name);
+
+    }
+
+}
