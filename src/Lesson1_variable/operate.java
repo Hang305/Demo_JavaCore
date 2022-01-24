6142f2cd15d30aecaf25e7e2d10005ae0ecb39b6
hang305 <hanglee305@gmail.com>
2022-01-24 15:52:56 +0700

    operate

---
+++ src/Lesson1_variable/operate.java
@@ -0,0 +1,18 @@
+package Lesson1_variable;
+
+public class operate {
+
+    public static void main(String[] args) {
+
+        int a = 5;
+
+        System.out.println(a++);
+        System.out.println(++a);
+
+        System.out.println(a+=1);
+
+        System.out.println(9==8);
+
+        System.out.println((char)('A'|' '));
+    }
+}
